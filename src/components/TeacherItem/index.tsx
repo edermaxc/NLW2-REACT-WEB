493a4e7727b48004092fd0ed36b6dd43861dcdff
Eder <edermaxc@gmail.com>
2020-08-13 11:57:31 -0300

    transmissao03

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,31 +2,52 @@ import React from 'react'
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+  "id": number;
+  "subject": string;
+  "cost": number;
+  "name": string;
+  "avatar": string;
+  "whatsapp": number;
+  "bio": string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> =  ({teacher}) => {
+
+  function createNewConnection(){
+      api.post('connections', {
+        user_id: teacher.id,
+      })
+    };
+
     return (
         <article className="teacher-item">
         <header>
-          <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHnsh2PFLuVpA/profile-displayphoto-shrink_200_200/0?e=1602720000&v=beta&t=E789fdq4PypR1h-EvQW-AwRaOXSGACX-CCC9GxQvCWQ" alt="Eder Colin"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Eder Colin</strong>
-            <span>Química</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
         <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br/><br/>
-          Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+          {teacher.bio}
         </p>
         <footer>
           <p>
             Preço/Hora
-            <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
           </p>
-          <button type="button">
+          <a target="_blank" onClick={createNewConnection} href={`https://wa.me/55${teacher.whatsapp}`}>
             <img src={whatsAppIcon} alt="WhatsApp"/>
             Entrar em contato
-          </button>
+          </a>
         </footer>
       </article>
 
